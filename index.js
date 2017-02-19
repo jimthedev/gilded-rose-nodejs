6f0a91449be62a790ff1ba2c27dc882ec1a1d661
Mike Frey <frey.mike@gmail.com>
2017-02-19 12:13:47 -0600

    cleaner

--- index.js
+++ index.js
@@ -20,52 +20,59 @@ class Inn {
   }
 
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+    this.items.forEach(item => {
+      switch(item.name) {
+        case '+5 Dexterity Vest':
+          return vestUpdate(item);
+        case 'Aged Brie':
+          return agiedBrieUpdate(item);
+        case 'Elixir of the Mongoose':
+          return elixirUpdate(item);
+        case 'Sulfuras, Hand of Ragnaros':
+          return sulfurasUpdate(item);
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          return backstagePassesUpdate(item);
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
+    })
   }
+
+}
+
+function vestUpdate(item) {
+  item.sellIn -= 1;
+  item.quality -= 1;
+  if (item.sellIn < 0) item.quality -= 1;
+  item.quality = Math.max(item.quality, 0);
+}
+
+function agiedBrieUpdate(item) {
+  item.sellIn -= 1;
+  if (item.quality >= 50) return;
+  item.quality += 1;
+  if (item.sellIn < 0 && item.quality < 50) {
+    item.quality += 1;
+  }
+}
+
+function elixirUpdate(item) {
+  item.sellIn -= 1;
+  if (item.quality <= 0) return;
+  item.quality -= 1;
+  if (item.quality <= 0) return;
+  if (item.sellIn <= 0) item.quality -= 1;
+}
+
+function sulfurasUpdate(item) {
+
+}
+
+function backstagePassesUpdate(item) {
+  item.sellIn -= 1;
+  item.quality += 1;
+  if (item.sellIn < 10) item.quality += 1;
+  if (item.sellIn < 5) item.quality += 1;
+  item.quality = Math.min(item.quality, 50);
+  if (item.sellIn < 0) item.quality = 0;
 }
 
 module.exports = Inn;
